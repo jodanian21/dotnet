af2819908fc332b92345fb143ba159008aff1478
Jodan Ian Gallego <jodan.gallego@upteamco.com>
2024-10-17 10:49:37 +0800

    Add JWT Generator

--- FoodTrip/FoodTrip.Infrastructure/DependencyInjection.cs
+++ FoodTrip/FoodTrip.Infrastructure/DependencyInjection.cs
@@ -1,12 +1,22 @@
-
+using FoodTrip.Application.Common.Interfaces.Authentication;
+using FoodTrip.Application.Common.Interfaces.Services;
+using FoodTrip.Infrastructure.Authentication;
+using FoodTrip.Infrastructure.Services;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 
 namespace FoodTrip.Infrastructure;
 
 public static class DependencyInjection
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration
+    )
     {
+        services.Configure<JwtSettings>(configuration.GetSection("JwtSettings"));
+        services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
+        services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
         return services;
     }
 }
